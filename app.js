7a23d836c9960eb9bfe709578731dc3e0a20e0ad
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-03-06 15:01:42 -0500

    updated views

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 
 const app = express();
+const mustache = mustacheExpress();
 
 app.use(express.static('www'));
+
+// setup mustache express
+mustache.cache = null;
+app.engine('mustache', mustache);
+app.set('view engine', 'mustache');
+
+// GET requests
+app.get('/home', (req, res) => {
+    res.render(__dirname + '/www/views/home'); // show home page
+});
+
+app.get('/create', (req, res) => {
+    res.render(__dirname + '/www/views/create-blog'); // show create-blog page
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render(__dirname + '/www/views/dashboard'); // show dashboard page
+});
+
+app.get('/about', (req, res) => {
+    res.render(__dirname + '/www/views/about'); // show about page
+});
 
 // start server
 const port = process.env.PORT || 3000;
