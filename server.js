39138a03ba8df2693de78df179311ab7c4106377
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-03-06 14:19:27 -0500

    added nodemon

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/www'));
-
-var port = process.env.PORT || 3000;
-
-app.listen(port);
-console.log('working on port ' + port);
