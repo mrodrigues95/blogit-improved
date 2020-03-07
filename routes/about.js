fb0e297791e975f89a3d24453fffb7662ba22b8a
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-03-07 16:26:34 -0500

    fixed routing for about page

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('www/views/about'); // show home page
+    res.render('about'); // show home page
 });
 
 module.exports = router;
